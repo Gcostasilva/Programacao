9348a7f65882fb58c1864d09a90f9808b10b232b
Gcostasilva <guimagoot73@gmail.com>
2026-08-28 16:56:55 -0300

    Inclui modal de busca de códigos na programação semanal

--- pages/programacao/semanal/index.php
+++ pages/programacao/semanal/index.php
@@ -3,10 +3,11 @@
     <div class="row mt-3 ">
         <div class="col-12">
             
-            <?php include 'pages\programacao\semanal\componentes\formulario.php'; ?>
+            <?php include 'pages\\programacao\\semanal\\componentes\\formulario.php'; ?>
             <?php include 'pages/programacao/semanal/componentes/modal_editar.php'; ?>
             <?php include 'pages/programacao/semanal/componentes/modalCalculo_chapa.php'; ?>
             <?php include 'pages/programacao/semanal/componentes/modalCalculo_perfil.php'; ?>
+            <?php include 'pages/programacao/semanal/componentes/modal_buscaCodigo.php'; ?>
         </div>
     </div>
 
