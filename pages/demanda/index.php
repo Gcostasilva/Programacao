c082ff929d7e9dfd44db69f77d6af8001327da25
Gcostasilva <guimagoot73@gmail.com>
2026-09-01 14:02:54 -0300

    Refatora Demanda para tabela reutilizavel

--- pages/demanda/index.php
+++ pages/demanda/index.php
@@ -1,9 +1,13 @@
+<?php
+$modoDemanda = 'pagina';
+$idTabelaDemanda = 'tb_demanda';
+?>
+
 <div class="container-fluid">
 
-    <div class="row mt-3 ">
+    <div class="row mt-3">
         <div class="col-12">
-            
-            <?php include 'pages\demanda\componente\filtro.php'; ?>
+            <?php include 'pages/demanda/componente/filtro.php'; ?>
         </div>
     </div>
 
