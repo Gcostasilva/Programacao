6c9175f3b1fb37c6fb116d61202e3b8093d8540c
Gcostasilva <guimagoot73@gmail.com>
2026-09-01 14:03:14 -0300

    Adiciona modal de selecao de Demanda na programacao semanal

---
+++ pages/programacao/semanal/componentes/modal_demanda.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="modalBuscaDemanda" tabindex="-1" aria-labelledby="modalBuscaDemandaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen-lg-down modal-xl modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalBuscaDemandaLabel">Selecionar Demanda</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+
+            <div class="modal-body">
+                <?php
+                $modoDemanda = 'selecao';
+                $idTabelaDemanda = 'tb_demanda_modal';
+                include 'pages/demanda/componente/tabela.php';
+                ?>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
