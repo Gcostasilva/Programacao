d686b7779838870011dbcbee372caa8b4508a425
Gcostasilva <guimagoot73@gmail.com>
2026-09-21 16:06:02 -0300

    Adiciona área de impressão da programação quinzenal

--- pages/programacao/quinzena/index.php
+++ pages/programacao/quinzena/index.php
@@ -1,6 +1,11 @@
 <div class="container-fluid">
 
-    <div class="row mt-3 ">
+    <div class="row mt-3">
+        <div class="col-12 d-flex justify-content-end mb-2 no-print">
+            <button type="button" class="btn btn-primary" id="btnImprimirProgramacaoQuinzena">
+                <i class="bi bi-printer"></i> Imprimir programação
+            </button>
+        </div>
         <div class="col-12">
             <?php include 'pages\programacao\quinzena\componentes\formulario.php'; ?>
         </div>
@@ -13,3 +18,14 @@
     </div>
 
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const botao = document.getElementById('btnImprimirProgramacaoQuinzena');
+    if (!botao) return;
+
+    botao.addEventListener('click', function () {
+        window.print();
+    });
+});
+</script>
