5823dadf0c061d12e9485deec33cd90f3a1a898f
Gcostasilva <guimagoot73@gmail.com>
2026-09-21 16:48:31 -0300

    Filtra códigos da programação semanal para 04 e 05

--- controllers/ProgSemanal_controller.php
+++ controllers/ProgSemanal_controller.php
@@ -3,28 +3,26 @@
 require_once __DIR__ . '/../models/RecursoModel.php';
 require_once __DIR__ . '/../models/VendedorModel.php';
 require_once __DIR__ . '/../models/tabelasModel.php';
-require_once __DIR__ . '/../models/ProgramacaoModel.php';
-require_once __DIR__ . '/../includes/helpers.php'; // ajuste o caminho conforme seu projeto
+require_once __DIR__ . '/../models/ProdutoModel.php';
+require_once __DIR__ . '/../includes/helpers.php';
 
 $recursoModel = new RecursoModel();
 $vendedorModel = new VendedorModel();
-$tabelaDiaria  = new tabelasModel();
+$tabelaDiaria = new tabelasModel();
+$produtoModel = new ProdutoModel();
 
 $dados = [];
 $tabela = [];
-$idDiaria = [];
-
-// Semana atual como padrão, igual já era feito na view
 $semana = $_GET['semana'] ?? date('o-\WW');
 $intervalo = calcularIntervaloSemana($semana);
 
 $dados['vendedores'] = $vendedorModel->listar();
 $dados['recursos_diario'] = $recursoModel->listarDiario();
 $dados['recursos_semanal'] = $recursoModel->listarSemanal();
+$dados['listaCodigos'] = $produtoModel->listarCodigosProgramacao();
 
 $tabela['tabDiaria'] = $tabelaDiaria->listarTabDiario();
 $tabela['tabSemanal'] = $tabelaDiaria->listarTabSemanal($intervalo['inicio'], $intervalo['fim']);
 $dados['prodSemanal'] = [];
-$dados['listaCodigos'] = $tabelaDiaria->listaCodigos();
 
 include __DIR__ . '/../pages/programacao/semanal/index.php';
