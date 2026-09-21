dc139b9075cf28c31050709fd5f90b10bf51b451
Gcostasilva <guimagoot73@gmail.com>
2026-09-21 20:18:54 -0300

    Adiciona modal de seleção de demanda para a programação quinzenal

---
+++ pages/programacao/quinzena/componentes/modal_demanda_selecao.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="modalBuscaDemandaQuinzena" tabindex="-1" aria-labelledby="modalBuscaDemandaQuinzenaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalBuscaDemandaQuinzenaLabel">
+                    <i class="bi bi-boxes me-2"></i>Selecionar Demanda
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+            <div class="modal-body p-2" id="modalBuscaDemandaQuinzenaBody">
+                <div class="text-center py-5">
+                    <div class="spinner-border" role="status"></div>
+                    <div class="mt-2">Carregando demanda...</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
