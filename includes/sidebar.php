93862cb10f57d55bd29cbbc31bb22b5fb8d3f134
Gcostasilva <guimagoot73@gmail.com>
2026-09-22 14:22:31 -0300

    Corrige estrutura e links da sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,119 +7,111 @@ $paginaAtual = $_GET['page'] ?? '';
 <aside class="app-sidebar shadow">
 
     <div class="sidebar-brand border-0 align-self-center align-content-center ps-md-3">
-
         <a href="index.php" class="brand-link">
-            <img src="uploads/img/Logotipo_eg
-
-            <span class=" brand-text fw-bold">
-            Perfinasa Metais
-            </span>
+            <img src="uploads/img/Logotipo_eg.png" alt="Perfinasa Metais" class="brand-image opacity-75 shadow">
+            <span class="brand-text fw-bold">Perfinasa Metais</span>
         </a>
-
     </div>
 
     <div class="sidebar-wrapper">
-
         <nav class="mt-2">
-
             <ul class="nav sidebar-menu flex-column"
                 data-lte-toggle="treeview"
                 data-accordion="true">
 
                 <?php foreach ($MENU as $item): ?>
-                    <?php foreach ($MENU as $item): ?>
 
-                        <?php
-                        $submenuAtivo = false;
+                    <?php
+                    $submenuAtivo = false;
 
-                        if (isset($item['submenu'])) {
+                    if (isset($item['submenu'])) {
+                        foreach ($item['submenu'] as $sub) {
+                            if (empty($sub['url'])) {
+                                continue;
+                            }
 
-                            foreach ($item['submenu'] as $sub) {
+                            parse_str(
+                                parse_url($sub['url'], PHP_URL_QUERY) ?? '',
+                                $parametros
+                            );
 
-                                parse_str(
-                                    parse_url($sub['url'], PHP_URL_QUERY),
-                                    $parametros
-                                );
-
-                                if (($parametros['page'] ?? '') === $paginaAtual) {
-                                    $submenuAtivo = true;
-                                    break;
-                                }
+                            if (($parametros['page'] ?? '') === $paginaAtual) {
+                                $submenuAtivo = true;
+                                break;
                             }
                         }
-                        ?>
+                    }
+                    ?>
 
-                        <?php if (isset($item['submenu'])): ?>
+                    <?php if (isset($item['submenu'])): ?>
 
-                            <li class="nav-item <?= $submenuAtivo ? 'menu-open' : '' ?>">
-
-                                #">
-
-                                <i class="nav-icon <?= $item['icone'] ?>"></i>
-
+                        <li class="nav-item <?= $submenuAtivo ? 'menu-open' : '' ?>">
+                            <a href="<?= htmlspecialchars($item['url'] ?? '#', ENT_QUOTES, 'UTF-8') ?>"
+                               class="nav-link <?= $submenuAtivo ? 'active' : '' ?>">
+                                <i class="nav-icon <?= htmlspecialchars($item['icone'], ENT_QUOTES, 'UTF-8') ?>"></i>
                                 <p>
-                                    <?= $item['titulo'] ?>
+                                    <?= htmlspecialchars($item['titulo'], ENT_QUOTES, 'UTF-8') ?>
                                     <i class="nav-arrow bi bi-chevron-right"></i>
                                 </p>
+                            </a>
 
-                                </a>
+                            <ul class="nav nav-treeview">
+                                <?php foreach ($item['submenu'] as $sub): ?>
 
-                                <ul class="nav nav-treeview">
+                                    <?php
+                                    $subAtivo = false;
 
-                                    <?php foreach ($item['submenu'] as $sub): ?>
-
-                                        <?php
+                                    if (!empty($sub['url'])) {
                                         parse_str(
-                                            parse_url($sub['url'], PHP_URL_QUERY),
+                                            parse_url($sub['url'], PHP_URL_QUERY) ?? '',
                                             $parametrosSub
                                         );
 
-                                        $subAtivo =
-                                            (($parametrosSub['page'] ?? '') === $paginaAtual);
-                                        ?>
+                                        $subAtivo = (($parametrosSub['page'] ?? '') === $paginaAtual);
+                                    }
+                                    ?>
 
-                                        <li class="nav-item">
-
-                                            <?= $sub['url'] ?> class="nav-link <?= $subAtivo ? 'active' : '' ?>">
-
+                                    <li class="nav-item">
+                                        <a href="<?= htmlspecialchars($sub['url'] ?? '#', ENT_QUOTES, 'UTF-8') ?>"
+                                           class="nav-link <?= $subAtivo ? 'active' : '' ?>">
                                             <i class="nav-icon bi bi-circle"></i>
+                                            <p><?= htmlspecialchars($sub['titulo'], ENT_QUOTES, 'UTF-8') ?></p>
+                                        </a>
+                                    </li>
 
-                                            <p><?= $sub['titulo'] ?></p>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
 
-                                            </a>
+                    <?php else: ?>
 
-                                        </li>
+                        <?php
+                        $itemAtivo = false;
 
-                                    <?php endforeach; ?>
+                        if (!empty($item['url'])) {
+                            parse_str(
+                                parse_url($item['url'], PHP_URL_QUERY) ?? '',
+                                $parametrosItem
+                            );
 
-                                </ul>
+                            $itemAtivo = (($parametrosItem['page'] ?? '') === $paginaAtual);
+                        }
+                        ?>
 
-                            </li>
+                        <li class="nav-item">
+                            <a href="<?= htmlspecialchars($item['url'] ?? '#', ENT_QUOTES, 'UTF-8') ?>"
+                               class="nav-link <?= $itemAtivo ? 'active' : '' ?>">
+                                <i class="nav-icon <?= htmlspecialchars($item['icone'], ENT_QUOTES, 'UTF-8') ?>"></i>
+                                <p><?= htmlspecialchars($item['titulo'], ENT_QUOTES, 'UTF-8') ?></p>
+                            </a>
+                        </li>
 
-                        <?php else: ?>
-
-                            <li class="nav-item">
-
-                                <?= $item['url'] ?> class="nav-link">
-
-                                <i class="nav-icon <?= $item['icone'] ?>"></i>
-
-                                <p><?= $item['titulo'] ?></p>
-
-                                </a>
-
-                            </li>
-
-                        <?php endif; ?>
-
-                    <?php endforeach; ?>
+                    <?php endif; ?>
 
                 <?php endforeach; ?>
 
             </ul>
-
         </nav>
-
     </div>
 
 </aside>
